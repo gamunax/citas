a95550bfa0d5da6329388d8954c10b3562fcf006
Jan Pierre <gamunaxx@gmail.com>
2021-03-21 16:54:19 -0500

    feat: add useState

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,24 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Formulario = () => {
+
+  const [appointment, updateAppointment] = useState({
+    pet: '',
+    owner: '',
+    date: '',
+    time: '',
+    symptoms: ''
+  });
+
+  const handleChange = (event) => {
+    updateAppointment({
+      ...appointment,
+      [event.target.name]: event?.target.value
+    });
+  };
+
+  const { pet, owner, date, time, symptoms } = appointment;
+
   return (
     <Fragment>
       <h2>Crear cita</h2>
@@ -8,9 +26,56 @@ const Formulario = () => {
         <label>Nombre de Mascota</label>
         <input
           type="text"
-          name="mascota"
+          name="pet"
           className="u-full-width"
+          placeholder="Nombre de mascota"
+          onChange={handleChange}
+          value={pet}
         />
+
+        <label>Nombre de Dueño</label>
+        <input
+          type="text"
+          name="owner"
+          className="u-full-width"
+          placeholder="Nombre de Duego de la mascota"
+          onChange={handleChange}
+          value={owner}
+        />
+
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="date"
+          className="u-full-width"
+          onChange={handleChange}
+          value={date}
+        />
+
+        <label>Hora</label>
+        <input
+          type="time"
+          name="time"
+          className="u-full-width"
+          onChange={handleChange}
+          value={time}
+        />
+
+        <label>Síntomas</label>
+        <textarea
+          className="u-full-width"
+          name="symptoms"
+          onChange={handleChange}
+          value={symptoms}
+        >
+        </textarea>
+
+        <button
+          type="submit"
+          className="u-full-width button-primary"
+        >
+          Agregar cita
+        </button>
       </form>
     </Fragment>
   );
